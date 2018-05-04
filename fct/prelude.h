ac5a726744adf947290825e779cfb6c5916963b7
z y <zachary_young@zerovector.space>
2018-05-04 09:45:37 -0700

    Add license

--- fct/prelude.h
+++ fct/prelude.h
@@ -1,3 +1,22 @@
+/*
+  This file is part of fct.
+
+  fct is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  fct is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with fct.  If not, see <http://www.gnu.org/licenses/>.
+
+  Copyright 2018 Zachary Young
+  */
+
 #ifndef functional_algorithms_prelude_header
 #define functional_algorithms_prelude_header
 
