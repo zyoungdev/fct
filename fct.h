b939d735f20ae46bd8732854fc6a292d815dd236
z y <zachary_young@zerovector.space>
2019-01-09 11:24:32 -0800

    Remove composition.h include, already sourced in prelude.h

--- fct.h
+++ fct.h
@@ -20,7 +20,6 @@
 #ifndef functional_algorithms_header
 #define functional_algorithms_header
 
-#include "fct/internal/composition.h"
 #include "fct/prelude.h"
 #include "fct/data/maybe.h"
 #include "fct/data/list.h"
