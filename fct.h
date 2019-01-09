0bffff26d64464d2e261fde1857ca5503420e215
z y <zachary_young@zerovector.space>
2019-01-09 11:19:58 -0800

    Correctly source composition.h

--- fct.h
+++ fct.h
@@ -20,7 +20,7 @@
 #ifndef functional_algorithms_header
 #define functional_algorithms_header
 
-#include "fct/composition.h"
+#include "fct/internal/composition.h"
 #include "fct/prelude.h"
 #include "fct/data/maybe.h"
 #include "fct/data/list.h"
